1e256586daba21d012c89875327f6d1223e36af1
Antoine EDDI <aeddi@e3r12p23.42.fr>
2016-04-18 16:51:59 +0200

    Strcmp WIP

--- sources/ft_strcmp.s
+++ sources/ft_strcmp.s
@@ -1,11 +1,27 @@
 section .text
 	global	ft_strcmp
+	extern	ft_strlen
 
 ft_strcmp:
 	push	rbp				; Set up a new stack frame
 	mov		rbp, rsp
 
+	push	rdi				; Save registers to stack (movsb gonna override them)
+	push	rsi
 
+	call	ft_strlen		; Get str length
+	mov		rcx, rax
+
+	cld						; Clear direction flag (left to right)
+	repe	cmpsb			; repe: Decrement rcx and repeat cmpsb while (rcx != 0 && ZEROFLAG)
+							; cmpsb: Check if byte[rdi] == byte[rsi] and set status flags, then increment rdi and rsi in the way indicated by the direction flag
+
+	xor		rax, rax
+	mov		al, byte[rdi - 1]
+	sub		al, byte[rsi - 1]
+
+	pop		rsi				; Restore registers from stack
+	pop		rdi
 
 	mov		rsp, rbp		; Remove stack frame and return 
 	pop		rbp
--- tests/sources/part4_strcmp.c
+++ tests/sources/part4_strcmp.c
@@ -1,7 +1,12 @@
 #include <test.h>
+#include <string.h>
 
 START_TEST(simple_test)
 {
+	ck_assert_int_eq(ft_strcmp("normal", "normal"), strcmp("normal", "normal"));
+	ck_assert_int_eq(ft_strcmp("moins", "mojns"), strcmp("moins", "mojns"));
+	ck_assert_int_eq(ft_strcmp("plus", "plts"), strcmp("plus", "plts"));
+	ck_assert_int_eq(ft_strcmp("", ""), strcmp("", ""));
 }
 END_TEST
 
@@ -9,4 +14,3 @@ void	tc_strcmp(TCase *tc_part4)
 {
 	tcase_add_test(tc_part4, simple_test);
 }
-
