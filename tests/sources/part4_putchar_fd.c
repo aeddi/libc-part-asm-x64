eca9ec4a0c08dce15febc307697446e437d56e94
Plastic <plastic@localhost.localdomain>
2016-04-19 19:25:47 +0200

    Add test for putnbr & putchar

--- tests/sources/part4_putchar_fd.c
+++ tests/sources/part4_putchar_fd.c
@@ -1,7 +1,30 @@
 #include <test.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
 
 START_TEST(simple_test)
 {
+	int		ret;
+	int		out;
+	int		p[2];
+	char	c;
+
+	out = dup(2);
+	pipe(p);
+	dup2(p[1], 2);
+
+	ft_putchar_fd('T', 2);
+
+	dup2(out, 2);
+	ret = read(p[0], &c, 1);
+
+	close(p[0]);
+	close(p[1]);
+	close(out);
+
+	ck_assert_int_eq(c, 'T');
 }
 END_TEST
 
@@ -9,4 +32,3 @@ void	tc_putchar_fd(TCase *tc_part4)
 {
 	tcase_add_test(tc_part4, simple_test);
 }
-
--- tests/sources/part4_putnbr_fd.c
+++ tests/sources/part4_putnbr_fd.c
@@ -1,7 +1,41 @@
 #include <test.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/uio.h>
+
+#define BUF_SZ 128
+
+void	write_read_check(int param, char *check)
+{
+	int		out;
+	int		p[2];
+	int		ret;
+	char	buf[BUF_SZ];
+
+	out = dup(2);
+	pipe(p);
+	dup2(p[1], 2);
+
+	ft_putnbr_fd(param, 2);
+	dup2(out, 2);
+	ret = read(p[0], buf, BUF_SZ);
+	buf[ret] = 0;
+
+	ck_assert_str_eq(buf, check);
+
+	close(p[0]);
+	close(p[1]);
+	close(out);
+}
 
 START_TEST(simple_test)
 {
+	write_read_check(123, "123");
+	write_read_check(1, "1");
+	write_read_check(0, "0");
+	write_read_check(-1, "-1");
+	write_read_check(-123, "-123");
 }
 END_TEST
 
