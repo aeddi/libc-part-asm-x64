b322ed575ed8c77823eba0969a8b9710bbc1f093
Plastic <plastic@localhost.localdomain>
2016-04-19 19:24:15 +0200

    puchar & putstr done

--- sources/ft_putchar_fd.s
+++ sources/ft_putchar_fd.s
@@ -1,3 +1,12 @@
+%ifdef LINUX				; Define right syscall index
+	%define	WRITE	1
+%elif DARWIN
+	%define	WRITE	0x2000004
+%endif
+
+section	.bss
+	char:	resb 1
+
 section .text
 	global	ft_putchar_fd
 
@@ -5,7 +14,13 @@ ft_putchar_fd:
 	push	rbp				; Set up a new stack frame
 	mov		rbp, rsp
 
+	mov		[rel char], dil	; Move char parameter into char var
 
+	mov		rax, WRITE		; Set write syscall index
+	mov		rdi, rsi		; Set fd parameter as first write parameter
+	lea		rsi, [rel char]	; Set char var pointer as second write parameter
+	mov		rdx, 1			; Set 1 as third write parameter (size of a char)
+	syscall					; Call write
 
 	mov		rsp, rbp		; Remove stack frame and return 
 	pop		rbp
--- sources/ft_putnbr_fd.s
+++ sources/ft_putnbr_fd.s
@@ -1,11 +1,51 @@
 section .text
 	global	ft_putnbr_fd
+	extern	ft_putchar_fd
 
 ft_putnbr_fd:
 	push	rbp				; Set up a new stack frame
 	mov		rbp, rsp
 
+	push	rbx				; Save registers to stack according x86-64 cedcl
 
+	mov		edx, edi		; Copy parameter into a safe register
+	test	edx, edx		; If int parameter is unsigned
+	jns		recursive_print ; Jump to recursive_print
+	neg		edx				; Else negate it
+	mov		edi, '-'		; Set '-' as first ft_putchar_fd parameter
+
+	push	rdx				; Save registers to stack before ft_putchar_fd call
+	push	rsi
+	call	ft_putchar_fd	; Call ft_putchar_fd
+	pop		rsi				; Save registers to stack before ft_putchar_fd call			
+	pop		rdx
+
+recursive_print:
+	cmp		edx, 9			; If int parameter > 9
+	ja		divide			; Jump to divide
+	mov		edi, edx		; Else set it as first ft_putchar_fd parameter
+	add		edi, '0'		; Add it '0' value (for ascii matching)
+	push	rsi				; Save register to stack before ft_putchar_fd call
+	call	ft_putchar_fd	; Call ft_putchar_fd
+	pop		rsi				; Restore register from stack after ft_putchar_fd call			
+	jmp		return			; Return
+
+divide:
+	mov		eax, edx		; Store int parameter in eax register
+	xor		edx, edx		; Set edx to 0
+	mov		ebx, 10			; Set ebx to 10
+	div		ebx				; Divide eax (int parameter) by ebx value (10).
+							; eax will contain the quotient (div result) and edx the remainder (modulo result)
+
+	push	rdx				; Save remainder to stack before ft_putnbr_fd recursive call
+	mov		edi, eax		; Set quotient as first ft_putnbr_fd parameter
+	call	ft_putnbr_fd	; Call ft_putnbr_fd recursively
+
+	pop		rdi				; Set remainder as first ft_putnbr_fd parameter
+	call	ft_putnbr_fd	; Call ft_putnbr_fd recursively
+
+return:
+	pop		rbx				; Restore registers from stack according x86-64 cedcl
 
 	mov		rsp, rbp		; Remove stack frame and return 
 	pop		rbp
